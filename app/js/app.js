d4da4b935e21622e799c000e42d0dd1ab1b3aab2
David Von Lehman <david.vonlehman@gmail.com>
2015-03-18 12:03:46 -0700

    More site updates

--- app/js/app.js
+++ app/js/app.js
@@ -60,18 +60,12 @@ angular.module('aerobatic-io').config(function($routeProvider, $locationProvider
     });
 });
 
-angular.module('aerobatic-io').run(function ($log, $rootScope, $location, analytics, content, aerobatic) {
+angular.module('aerobatic-io').run(function ($window, $log, $rootScope, $location, analytics, content, aerobatic) {
   $log.info("Angular app aerobatic-io run event");
   analytics.initialize();
   content.initialize();
 
-  // Preserve the querystring during HTML5 view navigations when in simulator
-  // mode. This way when livereload forces the browser to refresh we won't lose
-  // the fact we are in simulator mode designeated by the "sim=1" in the querystring.
-  var originalQuery = $location.search();
   $rootScope.$on('$routeChangeStart', function() {
-    for (var key in originalQuery) {
-      $location.search(key, originalQuery[key]);
-    }
+    $window.scrollTo(0, 0);
   });
 });
